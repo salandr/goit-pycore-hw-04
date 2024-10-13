236a0ed59b801bf7f72b10332daf73a922c14134
salandr <ansaliy200013@gmail.com>
2024-10-13 22:53:09 +0100

    tasks solved

---
+++ task_3/main.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+from colorama import init, Fore
+
+init(autoreset=True)
+
+
+def display_directory_structure(directory_path, indent=0):
+    path = Path(directory_path)
+
+    if not path.exists() or not path.is_dir():
+        print(Fore.RED + f"Шлях '{directory_path}' не існує або не є директорією.")
+        return
+
+    print(" " * indent + Fore.BLUE + f"📁 {path.name}/")
+
+    for item in path.iterdir():
+        if item.is_dir():
+            print(" " * (indent + 2) + Fore.CYAN + f"📁 {item.name}/")
+            display_directory_structure(item, indent + 4)
+        elif item.is_file():
+            print(" " * (indent + 2) + Fore.GREEN + f"📄 {item.name}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(Fore.RED + "Потрібно вказати шлях до директорії.")
+        sys.exit(1)
+
+    directory_path = sys.argv[1]
+
+    display_directory_structure(directory_path)
